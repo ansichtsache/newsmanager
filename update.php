62004fdf99ad69ea1dcdc755fbbc87dc3c8748c5
Tizian <t.bauer@ansichtsache.com>
2018-03-16 20:06:59 +0100

    included install.php

--- update.php
+++ update.php
@@ -4,7 +4,9 @@
 
 // Diese Datei wird beim Update eines Addons über den Installer aufgerufen
 
-// Hier können zum Beispiel DB-Tabellen angepasst werden
+// Anpassung der DB-Tabellen jetzt über die sql_table-Api in der install.php, die hier eingebunden wird. Funktioniert erst ab Redaxo 5.4
+
+$this->includeFile('install.php');
 
 // $this->getVersion() liefert die noch aktuell installierte Version
 
@@ -16,11 +18,6 @@ if (rex_string::versionCompare($this->getVersion(), '1.2', '<')) {
     // Änderungen für Nutzer die von Versionen kleiner 1.2 kommen
 }
 
-// DB-Anpassungen:
-// rex_sql_table::get(rex::getTable('my_table'))
-//     ->ensureColumn(new rex_sql_column('new_column', 'varchar(255)'))
-//     ->alter()
-// ;
 
 // Update kann abgebrochen werden:
 // throw new rex_functional_exception('Fehlermeldung');
